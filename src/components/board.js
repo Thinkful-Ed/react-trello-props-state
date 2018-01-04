7d95638ffdd67a32adcc1341b23a4eef8dcdbfd2
Joe Turner <joe@oampo.co.uk>
2018-01-04 13:12:40 +0000

    Prettier

--- src/components/board.js
+++ src/components/board.js
@@ -5,32 +5,32 @@ import List from './list';
 import './board.css';
 
 export default class Board extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      lists: [{
-            title: 'Example list 1'
-        },{
-            title: 'Example list 2'
-        }
-      ]
-    };
-  }
-  render() {
-    const lists = this.state.lists.map((list, index) => 
-        <li className="list-wrapper" key={index}>
-            <List {...list}/>
-        </li>
-    );
+        this.state = {
+            lists: [
+                {
+                    title: 'Example list 1'
+                },
+                {
+                    title: 'Example list 2'
+                }
+            ]
+        };
+    }
+    render() {
+        const lists = this.state.lists.map((list, index) => (
+            <li className="list-wrapper" key={index}>
+                <List {...list} />
+            </li>
+        ));
 
-    return (
-        <div className="board">
-            <h2>Example board</h2>
-            <ul className="lists">
-                {lists}
-            </ul>
-        </div>
+        return (
+            <div className="board">
+                <h2>Example board</h2>
+                <ul className="lists">{lists}</ul>
+            </div>
         );
     }
 }
